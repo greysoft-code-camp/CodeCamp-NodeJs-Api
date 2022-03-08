e8da0900001f54c9a10b53d1e0432558de1b694a
fatiudeen <shehufatiudeen@gmail.com>
2022-03-08 14:40:39 +0100

    update

--- src/api/v1/controllers/board.controller.js
+++ src/api/v1/controllers/board.controller.js
@@ -1,71 +1,79 @@
-import Board from "../models/Board.model";
+import Board from '../models/Board.model.js';
 
-   export const create = async(req,res) =>{
-        //this.clear();
-        let boardData = req.body;
-        let newBoard = new Board(boardData);
-        newBoard.save().then(board=>{
-            res.status(201).send({msg:"board created successfully",data:board})
-        }).catch(error=>{
-            console.log(error)
-            res.status(201).send({msg:"Unable to save board",data:error})
-        })
-        
-    }
-    export const update = async (req,res)=>{
-        Board.findOneAndUpdate({_id: req.params.boardId},req.body,{new: true}).then(board => {
-            if(!board) {
-                return res.status(404).send({
-                    message: "board not found with id " + req.params.boardId
-                });
-            }
-            else{
-                    res.status(200).send({msg:"Board Updated Successfully",data:board});
-            }
-        }).catch(err => {
-            console.log(err)
-            if(err.kind === 'ObjectId') {
-                return res.status(404).send({
-                    message: "board not found with id " + req.params.boardId
-                });                
-            }
-            return res.status(500).send({
-                message: "Error updating board with id " + req.params.boardId
-            });
+export const create = async (req, res) => {
+  //this.clear();
+  let boardData = req.body;
+  let newBoard = new Board(boardData);
+  newBoard
+    .save()
+    .then((board) => {
+      res.status(201).send({ msg: 'board created successfully', data: board });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(201).send({ msg: 'Unable to save board', data: error });
+    });
+};
+export const update = async (req, res) => {
+  Board.findOneAndUpdate({ _id: req.params.boardId }, req.body, { new: true })
+    .then((board) => {
+      if (!board) {
+        return res.status(404).send({
+          message: 'board not found with id ' + req.params.boardId,
         });
-    },
-
-   
-   
-    export const getById = async(req,res)=>{
-        Board.findOne({_id:req.params.boardId}).then(board=>{
-            if(board)
-               res.status(200).send({msg: "Board Fetched Successfully", data:board});
-            else
-               res.status(404).send({msg: "Board Not Found", data:board});
-
-        }).catch(error=>{
-            res.status(400).send({msg: "Unable to fetch boards", data:error});
+      } else {
+        res
+          .status(200)
+          .send({ msg: 'Board Updated Successfully', data: board });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      if (err.kind === 'ObjectId') {
+        return res.status(404).send({
+          message: 'board not found with id ' + req.params.boardId,
         });
-    }
+      }
+      return res.status(500).send({
+        message: 'Error updating board with id ' + req.params.boardId,
+      });
+    });
+};
 
+export const getById = async (req, res) => {
+  Board.findOne({ _id: req.params.boardId })
+    .then((board) => {
+      if (board)
+        res
+          .status(200)
+          .send({ msg: 'Board Fetched Successfully', data: board });
+      else res.status(404).send({ msg: 'Board Not Found', data: board });
+    })
+    .catch((error) => {
+      res.status(400).send({ msg: 'Unable to fetch boards', data: error });
+    });
+};
 
-    export const getAll = async(req,res)=>{
-        Board.find().then(boards=>{
-            res.status(200).send({msg: "Boards Fetched Successfully", data:boards});
-        }).catch(error=>{
-            res.status(400).send({msg: "Unable to fetch boards", data:error});
-        });
-    }
+export const getAll = async (req, res) => {
+  Board.find()
+    .then((boards) => {
+      res
+        .status(200)
+        .send({ msg: 'Boards Fetched Successfully', data: boards });
+    })
+    .catch((error) => {
+      res.status(400).send({ msg: 'Unable to fetch boards', data: error });
+    });
+};
 
-    export const del = async(req,res)=>{
-        Board.findOneAndDelete({_id:req.params.boardId}).then(boards=>{
-            res.status(200).send({msg: "Boards Fetched Successfully", data:boards});
-        }).catch(error=>{
-            res.status(400).send({msg: "Unable to fetch boards", data:error});
-        });
-    }
-
-   
-
-
+export const del = async (req, res) => {
+  Board.findOneAndDelete({ _id: req.params.boardId })
+    .then((boards) => {
+      res
+        .status(200)
+        .send({ msg: 'Boards Fetched Successfully', data: boards });
+    })
+    .catch((error) => {
+      res.status(400).send({ msg: 'Unable to fetch boards', data: error });
+    });
+};
--- src/api/v1/routes/board.route.js
+++ src/api/v1/routes/board.route.js
@@ -1,27 +1,25 @@
-import express from "express"
-const router =  express.Router()
-import * as boardController from '../controllers/board.controller';
+import express from 'express';
+const router = express.Router();
+import * as boardController from '../controllers/board.controller.js';
 
-
-router.post('/', (req,res) =>{
-    boardController.create(req,res);
+router.post('/', (req, res) => {
+  boardController.create(req, res);
 });
 
-router.get('/', (req,res) =>{
-    boardController.getAll(req,res);
+router.get('/', (req, res) => {
+  boardController.getAll(req, res);
 });
 
-router.put('/:boardId', (req,res) =>{
-    boardController.update(req,res);
+router.put('/:boardId', (req, res) => {
+  boardController.update(req, res);
 });
 
-router.get('/:boardId', (req,res) =>{
-    boardController.getById(req,res);
+router.get('/:boardId', (req, res) => {
+  boardController.getById(req, res);
 });
 
-router.delete('/:boardId', (req,res) =>{
-    boardController.del(req,res);
+router.delete('/:boardId', (req, res) => {
+  boardController.del(req, res);
 });
 
-
-export default router
+export default router;
